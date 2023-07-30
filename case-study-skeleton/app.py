5bdf5ba53d2ccfad2942a9391783a716e6d1587a
adikh24-meet <139681014+adikh24-meet@users.noreply.github.com>
2023-07-30 16:18:31 +0300

    a

--- case-study-skeleton/app.py
+++ case-study-skeleton/app.py
@@ -34,5 +34,9 @@ def signin():
 def apply():
     return render_template('apply.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
