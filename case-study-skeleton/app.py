46e9a1a26c88febe9e2218da909f7a4295acc389
EmilyPo24-meet <figidave659@gmail.com>
2023-07-30 15:46:56 +0300

    .

--- case-study-skeleton/app.py
+++ case-study-skeleton/app.py
@@ -2,6 +2,20 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+config = {
+    "apiKey": "AIzaSyCCnt5xrueFHh-XyLPWyP6XUZRkpi8tGco",
+    "authDomain": "personalproj-ba3c3.firebaseapp.com",
+    "projectId": "personalproj-ba3c3",
+    "storageBucket": "personalproj-ba3c3.appspot.com",
+    "messagingSenderId": "442365273351",
+    "appId": "1:442365273351:web:ade388b566ce5b2ebb91a2",
+    "databaseURL": "https://y2groupproject-4ae72-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
